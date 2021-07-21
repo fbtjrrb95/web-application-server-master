6da9f4c93016ed3ea6afb91d02ed320e891875bb
mac <tjrrb95@gmail.com>
2021-07-22 00:30:47 +0900

    Initial commit

---
+++ src/main/java/webserver/RequestHandler.java
@@ -0,0 +1,68 @@
+package webserver;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RequestHandler extends Thread {
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+
+    private Socket connection;
+
+    public RequestHandler(Socket connectionSocket) {
+        this.connection = connectionSocket;
+    }
+
+    public void run() {
+        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
+                connection.getPort());
+
+        try (InputStream in = connection.getInputStream();
+             OutputStream out = connection.getOutputStream() ) {
+
+            DataOutputStream dos = new DataOutputStream(out);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+
+            byte[] body = toBytes(br);
+            response200Header(dos, body.length);
+            responseBody(dos, body);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void responseBody(DataOutputStream dos, byte[] body) {
+        try {
+            dos.write(body, 0, body.length);
+            dos.flush();
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private byte[] toBytes(BufferedReader br) throws IOException {
+
+        String line = br.readLine();
+
+        String[] tokens = line.split(" ");
+        String url = tokens[1];
+        if("/index.html".equals(url)){
+            return Files.readAllBytes(new File("./webapp" + url).toPath());
+        }
+        return "Hello World".getBytes();
+    }
+}
