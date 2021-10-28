1b79a4d7d198efd89e98d7925be522272b753730
seokkyu <tjrrb95@gmail.com>
2021-10-29 00:04:22 +0900

    feat(refactor_httpResponse): httpResponse header 관련 코드 로직 구현

--- src/main/java/service/HttpResponse.java
+++ src/main/java/service/HttpResponse.java
@@ -3,62 +3,58 @@ package service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.print.attribute.HashPrintJobAttributeSet;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HttpResponse {
 
     private static final Logger log = LoggerFactory.getLogger(HttpResponse.class);
 
-    private OutputStream outputStream;
+    private final String protocol = "HTTP/1.1";
     private DataOutputStream dataOutputStream;
+    private String statusLine;
+    private Map<String, String> responseHeaderMap;
 
     public HttpResponse(OutputStream outputStream) {
-        this.outputStream = outputStream;
-        this.dataOutputStream = new DataOutputStream(outputStream);
+        dataOutputStream = new DataOutputStream(outputStream);
+        responseHeaderMap = new HashMap<>();
     }
 
     public void sendRedirect(String s) {
-
+        responseHeaderMap.put("Location", s);
     }
 
-    public void addHeader(String s, String s1) {
-
+    public void addHeader(String key, String value) {
+        responseHeaderMap.put(key, value);
     }
 
     public void forward(String s) {
 
     }
 
-    public void forwardBody(String s) {
+    public void response200Header(int i) {
+        statusLine = String.format("%s 200 OK", protocol);
+        responseHeaderMap.put("Content-Type", "text/html;charset=utf-8");
+        responseHeaderMap.put("Content-Length", String.valueOf(i));
+    }
+
+    public void responseBody(byte[] bytes) {
 
     }
 
-    public void response200Header(int i) {
+    public void processHeaders() {
         try {
-            dataOutputStream.writeBytes("HTTP/1.1 200 OK \r\n");
-            dataOutputStream.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dataOutputStream.writeBytes("Content-Length: " + i + "\r\n");
+            dataOutputStream.writeBytes(String.format("%s\r\n", statusLine));
+            for (Map.Entry<String, String> e : responseHeaderMap.entrySet()) {
+                dataOutputStream.writeBytes(String.format("%s: %s\r\n", e.getKey(), e.getValue()));
+            }
             dataOutputStream.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
         }
-    }
-
-    public void responseBody(byte[] bytes) {
-        try {
-            if (bytes == null) {
-                return;
-            }
-            dataOutputStream.write(bytes, 0, bytes.length);
-            dataOutputStream.flush();
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    public void processHeaders() {
-
     }
 }
