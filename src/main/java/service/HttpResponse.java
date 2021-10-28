754cb8b5417b9de67090cfbc1e189c251cea2ac6
seokkyu <tjrrb95@gmail.com>
2021-10-28 23:28:01 +0900

    feat(refactor_httpResponse): httpResponse 스켈레톤 코드 추가

--- src/main/java/service/HttpResponse.java
+++ src/main/java/service/HttpResponse.java
@@ -5,14 +5,36 @@ import java.io.OutputStream;
 public class HttpResponse {
 
     private OutputStream outputStream;
-    
+
     public HttpResponse(OutputStream outputStream) {
         this.outputStream = outputStream;
     }
 
     public void sendRedirect(String s) {
+
     }
 
     public void addHeader(String s, String s1) {
+        
+    }
+
+    public void forward(String s) {
+
+    }
+
+    public void forwardBody(String s) {
+
+    }
+
+    public void response200Header(int i) {
+
+    }
+
+    public void responseBody(byte[] bytes) {
+
+    }
+
+    public void processHeaders() {
+
     }
 }
