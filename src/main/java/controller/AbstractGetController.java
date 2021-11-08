ad209a3cfb5fbdfd15fb3178955c38d97cf52992
seokkyu <tjrrb95@gmail.com>
2021-11-08 22:10:54 +0900

    feat(polymorphism_controller): abstractGetController, ListUserController 구현

---
+++ src/main/java/controller/AbstractGetController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import service.HttpRequest;
+import service.HttpResponse;
+
+import java.io.IOException;
+
+public abstract class AbstractGetController implements Controller{
+    @Override
+    public void service(HttpRequest httpRequest, HttpResponse httpResponse) {
+        doGet(httpRequest, httpResponse);
+    }
+
+    public abstract void doGet(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException;
+}
---
+++ src/main/java/controller/ListUserController.java
@@ -0,0 +1,62 @@
+package controller;
+
+import db.DataBase;
+import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import service.HttpRequest;
+import service.HttpResponse;
+import util.HttpRequestUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.Map;
+
+public class ListUserController extends AbstractGetController {
+
+    private static final Logger log = LoggerFactory.getLogger(ListUserController.class);
+
+    @Override
+    public void doGet(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
+        String cookies = httpRequest.getHeader("Cookie");
+        Map<String, String> cookiesMap = HttpRequestUtils.parseCookies(cookies);
+
+        log.debug("cookies : {}", cookies);
+
+        boolean isLogined = Boolean.parseBoolean(cookiesMap.get("logined"));
+
+        // 로그인이 되어 있으면 유저 리스트를 반환
+        // 되어있지 않으면 로그인화면 반환
+        if (!isLogined) {
+            httpResponse.responseResource("/user/login.html");
+            return;
+        }
+
+        byte[] body = getUserList().getBytes(StandardCharsets.UTF_8);
+        httpResponse.responseResource(body);
+    }
+
+    private String getUserList() {
+        Collection<User> users = DataBase.findAll();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("<table border='1'>");
+        stringBuilder.append("<tr>");
+        stringBuilder.append("<td>userId</td>");
+        stringBuilder.append("<td>userName</td>");
+        stringBuilder.append("<td>userEmail</td>");
+        stringBuilder.append("</tr>");
+
+        users.forEach(user -> {
+            stringBuilder.append("<tr>");
+            stringBuilder.append(String.format("<td>%s</td>", user.getUserId()));
+            stringBuilder.append(String.format("<td>%s</td>", user.getName()));
+            stringBuilder.append(String.format("<td>%s</td>", user.getEmail()));
+            stringBuilder.append("</tr>");
+        });
+        stringBuilder.append("</table>");
+
+        return stringBuilder.toString();
+    }
+}
