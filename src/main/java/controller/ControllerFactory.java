3cb2bf52228d403a880f6b179bc80fcdee97ec53
seokkyu <tjrrb95@gmail.com>
2021-11-10 22:00:08 +0900

    feat(polymorphism_controller): 다형성 사용하여 기존 코드 리팩토링

--- src/main/java/controller/ControllerFactory.java
+++ src/main/java/controller/ControllerFactory.java
@@ -1,2 +1,32 @@
-package controller;public class ControllerFactory {
+package controller;
+
+public class ControllerFactory {
+
+    private static SimpleGetController simpleGetController = new SimpleGetController();
+    private static LoginController loginController = new LoginController();
+    private static ListUserController listUserController = new ListUserController();
+    private static CreateUserController createUserController = new CreateUserController();
+
+    private static ControllerFactory controllerFactory = new ControllerFactory();
+
+    private ControllerFactory () {}
+
+    public static Controller getInstance(String requestPath) {
+
+        if ("/user/login".equals(requestPath)) {
+            return loginController;
+        }
+
+        if ("/user/create".equals(requestPath)) {
+            return createUserController;
+        }
+
+        if ("/user/list".equals(requestPath)) {
+            return listUserController;
+        }
+
+        return simpleGetController;
+    }
+
+
 }
