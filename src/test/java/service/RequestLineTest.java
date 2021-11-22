555336c57dab69153816c4b856b3867c88543f67
seokkyu <tjrrb95@gmail.com>
2021-11-22 22:53:08 +0900

    feat(refactor_code): httpRequest path, params test code 추가

--- src/test/java/service/RequestLineTest.java
+++ src/test/java/service/RequestLineTest.java
@@ -2,7 +2,10 @@ package service;
 
 import org.junit.Test;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 
 
 public class RequestLineTest {
@@ -13,6 +16,20 @@ public class RequestLineTest {
         RequestLine requestLine = new RequestLine("GET /index.html HTTP/1.1");
         assertThat(requestLine.getMethod()).isEqualTo("GET");
         assertThat(requestLine.getPath()).isEqualTo("/index.html");
+
+    }
+
+    @Test
+    public void create_path_and_params() {
+        RequestLine requestLine = new RequestLine("GET /user/create?userId=javajigi&password=pass HTTP/1.1");
+
+        assertThat(requestLine.getMethod()).isEqualTo("GET");
+        assertThat(requestLine.getPath()).isEqualTo("/user/create");
+
+        Map<String, String> params = requestLine.getParams();
+        assertThat(params).hasSize(2)
+                .contains(entry("userId", "javajigi"), entry("password", "pass"));
+
 
     }
 
